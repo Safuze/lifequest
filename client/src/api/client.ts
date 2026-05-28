985cef55a415bcbe9747da804a9d345d5d7ba5e1
Chertov Denis <rainydaypinpaf@gmail.com>
2026-05-28 23:06:28 +0300

    add env client

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://lifequest-production-eb2d.up.railway.app/api/v1'
-
+const API_URL = import.meta.env.VITE_API_URL
 const apiClient = axios.create({
   baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },
